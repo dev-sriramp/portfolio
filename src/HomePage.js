422c5b02f7c2c890469cc21655e2c788e3a1e9bc
uniqueredhat <uniqueredhat@gmail.com>
2021-05-18 19:00:12 +0530

    removed

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,6 @@
-import React, { Component,useState } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import login from './Login'
+
 class HomePage extends Component {
   
   constructor(props) {
