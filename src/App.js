dd4995a98f7013c7f609db5de866ca3e4fb99e2d
uniqueredhat <uniqueredhat@gmail.com>
2021-05-18 18:44:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component, } from 'react';
 import './App.css';
-
-function App() {
+import HomePage from './HomePage';
+import Login from './Login'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from "react-router-dom";
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route exact path="/home">
+          <HomePage />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+  </Router>
+  );}
 }
 
 export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,55 @@
+import React, { Component,useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import login from './Login'
+class HomePage extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(){
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  render() {
+    const shown = (this.state.show) ? "show" : "" ;
+    return (
+      
+      <div >
+         <header className='jumbotron'>
+        <nav className='navbar navbar-light navbar-expand-sm bg-light'>
+          <div className='container'>
+            <button className='navbar-toggler' type='button' onClick={this.toggleMenu}>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <a className='navbar-brand mx-5' href='#'><h1>SRIRAM P</h1>
+              <h3>enthusiastic learner</h3>
+            </a>
+            <div className={"collapse navbar-collapse " + shown}>
+              <ul className='navbar-nav mx-5 px-5'>
+                <li className='nav-item active px-5'><a href='#' className='nav-link'>HOME</a></li>
+                <li className='nav-item px-5'><a href='#' className='nav-link'>RESUME</a></li>
+                <li className='nav-item px-5'><a href='#' className='nav-link'>PROJECT</a></li>
+                <li className='nav-item px-5'><a href='#' className='nav-link'>CONTACT</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        </header>
+      <div >
+        <h1>hi</h1>
+      </div>
+        
+      </div>
+    );
+  }
+}
+
+export default HomePage;
