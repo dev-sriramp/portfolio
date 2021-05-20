4a2b57c20b31e2f0787e7fc1b1046ca0564bd4aa
uniqueredhat <uniqueredhat@gmail.com>
2021-05-21 01:10:47 +0530

    corrected container in header and body

--- src/HomePage.js
+++ src/HomePage.js
@@ -31,7 +31,7 @@ class HomePage extends Component {
 
       <div >
         <header className='jumbotron'>
-          <nav className='navbar navbar-light navbar-expand-sm'>
+          <nav className='navbar navbar-light navbar-expand-md'>
             <div className='container'>
               <button className='navbar-toggler' type='button' onClick={this.toggleMenu}>
                 <span className='navbar-toggler-icon'></span>
@@ -40,11 +40,11 @@ class HomePage extends Component {
                 <h3>EnThUsIaStIc LeArNeR</h3>
               </a>
               <div className={"collapse navbar-collapse " + shown}>
-                <ul className='navbar-nav mx-5 px-5'>
-                  <li className='nav-item px-5 move' onClick={this.find('myresume')}><p>HOME</p></li>
-                  <li className='nav-item px-5 move' onClick={this.find('myresume')}><p>RESUME</p></li>
-                  <li className='nav-item px-5 move' onClick={this.find('myresume')}><p>PROJECT</p></li>
-                  <li className='nav-item px-5 move' onClick={this.find('myresume')}><p>CONTACT</p></li>
+                <ul className='navbar-nav'>
+                  <li className='nav-item move' onClick={this.find('myresume')}><p>HOME&emsp;</p></li>
+                  <li className='nav-item move' onClick={this.find('myresume')}><p>RESUME&emsp;</p></li>
+                  <li className='nav-item move' onClick={this.find('myresume')}><p>PROJECT&emsp;</p></li>
+                  <li className='nav-item move' onClick={this.find('myresume')}><p>CONTACT&emsp;</p></li>
                 </ul>
               </div>
             </div>
@@ -52,10 +52,10 @@ class HomePage extends Component {
         </header>
         <div className='container bg-light'>
           <div className='row'>
-            <div className='col-sm-12 col-md-6'>
+            <div className='col-sm-12 col-md-5'>
               <img src={Img} alt="pic" className='mine rounded-circle p-5' />
             </div>
-            <div className='col-sm-12 col-md-6'>
+            <div className='col-sm-12 col-md-7 '>
               <span className="move" onClick={this.find('myresume')}>
                 <svg className="bd-placeholder-img rounded-circle" width="180" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>MY RESUME</title><rect width="100%" height="100%" fill="#e55773" ></rect><text x="25%" y="50%" fill="#ffffff">MY RESUME</text></svg>
               </span>
