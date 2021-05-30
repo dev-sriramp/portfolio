99549c1b88801485e696726e758de932643d156c
uniqueredhat <uniqueredhat@gmail.com>
2021-05-30 23:46:05 +0530

    added some stuffs

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'element-react';
 import './HomePage.css';
 import Img from './assets/Port_Image.jpg';
 
@@ -23,7 +24,7 @@ class HomePage extends Component {
     console.log('find')
     if (siteValue === 'myresume') {
       console.log(siteValue);
-       window.location.href = '/error'
+      window.location.href = '/error'
     }
   }
   render() {
@@ -57,8 +58,8 @@ class HomePage extends Component {
               <img src={Img} alt="pic" className='mine rounded-circle p-5' />
             </div>
             <div className='col-sm-12 col-md-7 '>
-              <h1>Hello,</h1>
-              <h4>a bit about me:</h4>
+              <h1 className='hlo'>Hello,</h1>
+              <h4 className='homeabout'>a bit about me:</h4>
               <span className="move" onClick={this.find('myresume')}>
                 <svg className="bd-placeholder-img rounded-circle" width="180" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>MY RESUME</title><rect width="100%" height="100%" fill="#e55773" ></rect><text x="25%" y="50%" fill="#ffffff">MY RESUME</text></svg>
               </span>
@@ -68,6 +69,12 @@ class HomePage extends Component {
               <span className="move" onClick={this.find('myresume')}>
                 <svg className="bd-placeholder-img rounded-circle" width="180" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>MY SKILLS</title><rect width="100%" height="100%" fill="#8d8720"></rect><text x="25%" y="50%" fill="#ffffff">MY SKILLS</text></svg>
               </span>
+              <p className='abtme'>Torrieux de bout d'viarge de verrat de mosus de saint-cimonaque de calvince d'enfant d'chienne de cibouleau de batèche de ciboire.Boswell de saint-ciboire de calvince de sacristi de calvinouche de p'tit Jésus de cochonnerie de sapristi de maudite marde de verrat de cibole.
+              </p>
+            </div>
+            <div className='col-12 footer'>
+
+              <h4 className="fa fa-envelope fa-5x bg-primary" aria-hidden="true"></h4>
             </div>
           </div>
         </div>
